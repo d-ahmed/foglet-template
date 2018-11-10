fe936aabb6b453653e5882b3c55e39a25410e220
Daniel AHMED <daniel.ahmedinfo@gmail.com>
2018-11-10 16:52:05 +0100

    Test with descriptorB

--- examples/medexample/main.js
+++ examples/medexample/main.js
@@ -7,7 +7,7 @@ const MAX_PEERS = 0;
 //myChart;
 const overlay = createSigma("overlay");
 // Creating peers and sigma nodes
-const max = 20;
+const max = 100;
 const peers = [];
 const delta = 2 * 1000
 for (let i = 0; i < max; i++) {
